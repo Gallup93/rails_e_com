49311e61379e9d39b1ec21924a8060bf35261ca3
Michael Gallup <Gallup93@icloud.com>
2020-07-11 14:02:17 -0600

    Add gems

---
+++ spec/factories/customers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :customer do
+    id { "" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    created_at { "MyString" }
+    updated_at { "MyString" }
+  end
+end
