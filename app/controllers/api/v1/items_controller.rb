8d8f7d71b987786ddbc5e39963fb198875bad379
Michael Gallup <Gallup93@icloud.com>
2020-07-15 18:53:54 -0600

    Add item 'update' functionality

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -14,6 +14,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.create(item_params))
   end
 
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
   def destroy
     render json: ItemSerializer.new(Item.find(params[:id]))
     Item.delete(params[:id])
